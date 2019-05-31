c1f41e8a7ae704f4b15f5e229a460cb63e38a8d8
601724 <601724@syswin.com>
2019-05-31 18:51:07 +0800

    提交代啊

--- src/pages/employee/employee.js
+++ src/pages/employee/employee.js
@@ -1,33 +1,121 @@
 import React from 'react';
-import { Row, Col, Table, Input, Button } from 'antd';
+import { Row, Divider ,Col, Table, Input, Button } from 'antd';
 import './employee.less'
+const columns=[
+  {
+    title:'ID',       //当前显示的名字
+    key:'id',         //
+    dataIndex:'id',    //
+  },
+  {
+    title:'姓名',       //当前显示的名字
+    key:'name',         //
+    dataIndex:'name',    //
+  },{
+    title:'联系方式',       //当前显示的名字
+    key:'tel',         //
+    dataIndex:"tel",    //
+  },{
+    title:'邮箱',       //当前显示的名字
+    key:'email',         //
+    dataIndex:'email',    //
+  },{
+    title:'操作',       //当前显示的名字
+    key:'action',         //
+    // dataIndex:'action',    //
+    render :(text,record) =>(
+      <span>
+        <a href="javascript:;">修改</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">详情</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">删除</a>
+      </span>
+    )
+  },
+]
+
 export default class Employee extends React.Component {
 
   state = {};
   render() {
-    const columns=[
+
+    const tableData =[
       {
-        title:'ID',       //当前显示的名字
-        key:'id',         //
-        dateIndex:'id',    //
+        id:'1',
+        name:'lidy',
+        tel:'25',
+        email:'lidongyan_cn@163.com',
+      
       },{
-        title:'姓名',       //当前显示的名字
-        key:'name',         //
-        dateIndex:'name',    //
+        id:'2',
+        name:'yyt',
+        tel:'25',
+        email:'yyt@163.com',
+       
       },{
-        title:'联系方式',       //当前显示的名字
-        key:'tel',         //
-        dateIndex:"tel",    //
+        id:'3',
+        name:'lidy',
+        tel:'25',
+        email:'lidongyan_cn@163.com',
+        
       },{
-        title:'邮箱',       //当前显示的名字
-        key:'email',         //
-        dateIndex:'email',    //
+        id:'4',
+        name:'yyt',
+        tel:'25',
+        email:'yyt@163.com',
+       
       },{
-        title:'操作',       //当前显示的名字
-        key:'id',         //
-        dateIndex:'id',    //
-      },
+        id:'5',
+        name:'lidy',
+        tel:'25',
+        email:'lidongyan_cn@163.com',
+        
+      },{
+        id:'6',
+        name:'yyt',
+        tel:'25',
+        email:'yyt@163.com',
+       
+      },{
+        id:'7',
+        name:'lidy',
+        tel:'25',
+        email:'lidongyan_cn@163.com',
+        
+      },{
+        id:'8',
+        name:'yyt',
+        tel:'25',
+        email:'yyt@163.com',
+       
+      },{
+        id:'9',
+        name:'lidy',
+        tel:'25',
+        email:'lidongyan_cn@163.com',
+       
+      },{
+        id:'10',
+        name:'yyt',
+        tel:'25',
+        email:'yyt@163.com',
+       
+      },{
+        id:'11',
+        name:'lidy',
+        tel:'25',
+        email:'lidongyan_cn@163.com',
+       
+      },{
+        id:'12',
+        name:'yyt',
+        tel:'25',
+        email:'yyt@163.com',
+        
+      }
     ]
+
     return (
       <div>
         <div>
@@ -41,9 +129,13 @@ export default class Employee extends React.Component {
           </Row>
         </div>
         <div>
-          <Table>
+          <Table 
+            bordered
+            rowKey = {record => record.id}
+            columns= {columns}
+            dataSource ={tableData}
+          />
 
-          </Table>
         </div>
       </div>
     )
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Card, Table, Modal, Button, message} from 'antd';
+import { Card, Table, Modal, Button, message } from 'antd';
 import axios from './../../axios/index'
 import Utils from './../../utils/utils';
-export default class BasicTable extends React.Component{
+export default class BasicTable extends React.Component {
 
-    state={
-        dataSource2:[]
+    state = {
+        dataSource2: []
     }
 
     params = {
-        page:1
+        page: 1
     }
 
-    componentDidMount(){
-        const data = [
-            {
-                id:'0',
-                userName:'Jack',
-                sex:'1',
-                state:'1',
-                interest:'1',
-                birthday:'2000-01-01',
-                address:'北京市海淀区奥林匹克公园',
-                time:'09:00'
+    componentDidMount() {
+        const data = [{
+                id: '0',
+                userName: 'Jack',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '2000-01-01',
+                address: '北京市海淀区奥林匹克公园',
+                time: '09:00'
             },
             {
                 id: '1',
@@ -45,7 +44,7 @@ export default class BasicTable extends React.Component{
                 time: '09:00'
             },
         ]
-        data.map((item,index)=>{
+        data.map((item, index) => {
             item.key = index;
         })
         this.setState({
@@ -55,25 +54,25 @@ export default class BasicTable extends React.Component{
     }
 
     // 动态获取mock数据
-    request = ()=>{
+    request = () => {
         let _this = this;
         axios.ajax({
-            url:'/table/list',
-            data:{
-                params:{
-                    page:this.params.page
+            url: '/table/list',
+            data: {
+                params: {
+                    page: this.params.page
                 }
             }
-        }).then((res)=>{
-            if(res.code == 0){
+        }).then((res) => {
+            if (res.code == 0) {
                 res.result.list.map((item, index) => {
                     item.key = index;
                 })
                 this.setState({
-                    dataSource2:res.result.list,
-                    selectedRowKeys:[],
-                    selectedRows:null,
-                    pagination: Utils.pagination(res,(current)=>{
+                    dataSource2: res.result.list,
+                    selectedRowKeys: [],
+                    selectedRows: null,
+                    pagination: Utils.pagination(res, (current) => {
                         _this.params.page = current;
                         this.request();
                     })
@@ -82,41 +81,40 @@ export default class BasicTable extends React.Component{
         })
     }
 
-    onRowClick = (record,index)=>{
+    onRowClick = (record, index) => {
         let selectKey = [index];
         Modal.info({
-            title:'信息',
-            content:`用户名：${record.userName},用户爱好：${record.interest}`
+            title: '信息',
+            content: `用户名：${record.userName},用户爱好：${record.interest}`
         })
         this.setState({
-            selectedRowKeys:selectKey,
+            selectedRowKeys: selectKey,
             selectedItem: record
         })
     }
 
     // 多选执行删除动作
-    handleDelete = (()=>{
+    handleDelete = (() => {
         let rows = this.state.selectedRows;
         let ids = [];
-        rows.map((item)=>{
+        rows.map((item) => {
             ids.push(item.id)
         })
         Modal.confirm({
-            title:'删除提示',
+            title: '删除提示',
             content: `您确定要删除这些数据吗？${ids.join(',')}`,
-            onOk:()=>{
+            onOk: () => {
                 message.success('删除成功');
                 this.request();
             }
         })
     })
 
-    render(){
-        const columns = [
-            {
-                title:'id',
-                key:'id',
-                dataIndex:'id'
+    render() {
+        const columns = [{
+                title: 'id',
+                key: 'id',
+                dataIndex: 'id'
             },
             {
                 title: '用户名',
@@ -127,21 +125,21 @@ export default class BasicTable extends React.Component{
                 title: '性别',
                 key: 'sex',
                 dataIndex: 'sex',
-                render(sex){
-                    return sex ==1 ?'男':'女'
+                render(sex) {
+                    return sex == 1 ? '男' : '女'
                 }
             },
             {
                 title: '状态',
                 key: 'state',
                 dataIndex: 'state',
-                render(state){
-                    let config  = {
-                        '1':'咸鱼一条',
-                        '2':'风华浪子',
-                        '3':'北大才子',
-                        '4':'百度FE',
-                        '5':'创业者'
+                render(state) {
+                    let config = {
+                        '1': '咸鱼一条',
+                        '2': '风华浪子',
+                        '3': '北大才子',
+                        '4': '百度FE',
+                        '5': '创业者'
                     }
                     return config[state];
                 }
@@ -182,74 +180,87 @@ export default class BasicTable extends React.Component{
         ]
         const selectedRowKeys = this.state.selectedRowKeys;
         const rowSelection = {
-            type:'radio',
+            type: 'radio',
             selectedRowKeys
         }
         const rowCheckSelection = {
             type: 'checkbox',
             selectedRowKeys,
-            onChange:(selectedRowKeys,selectedRows)=>{
+            onChange: (selectedRowKeys, selectedRows) => {
                 this.setState({
                     selectedRowKeys,
                     selectedRows
                 })
             }
         }
-        return (
-            <div>
-                <Card title="基础表格">
-                    <Table 
-                        bordered
-                        columns={columns}
-                        dataSource={this.state.dataSource}
-                        pagination={false}
-                    />
-                </Card>
-                <Card title="动态数据渲染表格-Mock" style={{margin:'10px 0'}}>
-                    <Table
-                        bordered
-                        columns={columns}
-                        dataSource={this.state.dataSource2}
-                        pagination={false}
-                    />
-                </Card>
-                <Card title="Mock-单选" style={{ margin: '10px 0' }}>
-                    <Table
-                        bordered
-                        rowSelection={rowSelection}
-                        onRow={(record,index) => {
-                            return {
-                                onClick:()=>{
-                                    this.onRowClick(record,index);
-                                }
-                            };
-                        }}
-                        columns={columns}
-                        dataSource={this.state.dataSource2}
-                        pagination={false}
-                    />
-                </Card>
-                <Card title="Mock-单选" style={{ margin: '10px 0' }}>
-                    <div style={{marginBottom:10}}>
-                        <Button onClick={this.handleDelete}>删除</Button>
-                    </div>
-                    <Table
-                        bordered
-                        rowSelection={rowCheckSelection}
-                        columns={columns}
-                        dataSource={this.state.dataSource2}
-                        pagination={false}
-                    />
-                </Card>
-                <Card title="Mock-表格分页" style={{ margin: '10px 0' }}>
-                    <Table
-                        bordered
-                        columns={columns}
-                        dataSource={this.state.dataSource2}
-                        pagination={this.state.pagination}
-                    />
-                </Card>
-            </div>
+        return ( <
+            div >
+            <
+            Card title = "基础表格" >
+            <
+            Table bordered columns = { columns }
+            dataSource = { this.state.dataSource }
+            pagination = { false }
+            /> < /
+            Card > <
+            Card title = "动态数据渲染表格-Mock"
+            style = {
+                { margin: '10px 0' }
+            } >
+            <
+            Table bordered columns = { columns }
+            dataSource = { this.state.dataSource2 }
+            pagination = { false }
+            /> < /
+            Card > <
+            Card title = "Mock-单选"
+            style = {
+                { margin: '10px 0' }
+            } >
+            <
+            Table bordered rowSelection = { rowSelection }
+            onRow = {
+                (record, index) => {
+                    return {
+                        onClick: () => {
+                            this.onRowClick(record, index);
+                        }
+                    };
+                }
+            }
+            columns = { columns }
+            dataSource = { this.state.dataSource2 }
+            pagination = { false }
+            /> < /
+            Card > <
+            Card title = "Mock-单选"
+            style = {
+                { margin: '10px 0' }
+            } >
+            <
+            div style = {
+                { marginBottom: 10 }
+            } >
+            <
+            Button onClick = { this.handleDelete } > 删除 < /Button> < /
+            div > <
+            Table bordered rowSelection = { rowCheckSelection }
+            columns = { columns }
+            dataSource = { this.state.dataSource2 }
+            pagination = { false }
+            /> < /
+            Card > <
+            Card title = "Mock-表格分页"
+            style = {
+                { margin: '10px 0' }
+            } >
+            <
+            Table bordered columns = { columns }
+            dataSource = { this.state.dataSource2 }
+            pagination = { this.state.pagination }
+            /> < /
+            Card > <
+            /div>
         );
     }
 }
